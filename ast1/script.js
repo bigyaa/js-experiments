05b498f9aef6294bc4bb29562797a71ce7ed4a4a
bigya <bigyabajracharya@gmail.com>
2019-04-18 10:08:19 +0545

    Add functioning next and previous icons

--- ast1/script.js
+++ ast1/script.js
@@ -1,9 +1,13 @@
 var image = document.getElementsByClassName('image');
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
 
 var IMAGE_WIDTH = 1000;
 var SLIDER_MAX = -(image.length * IMAGE_WIDTH);
 var left = 0;
 var direction = 0;
+var indicatorNumber = 0;
+var flag = 0;
 
 slider.style.width = -SLIDER_MAX + 'px';
 
@@ -18,13 +22,42 @@ function slide() {
       direction = -10;
     }
 
+    previous.onclick = function(event) {
+      if (left <= -IMAGE_WIDTH) {
+        left = left + IMAGE_WIDTH;
+        slider.style.left = left + 'px';
+      }
+    };
+
+    next.onclick = function(event) {
+      if (left >= SLIDER_MAX + IMAGE_WIDTH) {
+        left = left - IMAGE_WIDTH;
+        slider.style.left = left + 'px';
+      }
+    };
+
     if (direction === -10 && left % IMAGE_WIDTH === 0) {
       clearInterval(interval);
       setTimeout(function() {
         slide();
-      }, 1000);
+      }, 2000);
     }
   }, 1000 / 60);
 }
 
+function createPageIndicators() {
+  var container = document.getElementById('sliderContainer');
+
+  var pageIndicators = document.createElement('div');
+  pageIndicators.setAttribute('id', 'pageIndicators');
+  container.appendChild(pageIndicators);
+
+  for (indicatorNumber = 0; indicatorNumber < image.length; indicatorNumber++) {
+    var indicator = document.createElement('button');
+    indicator.setAttribute('class', 'indicator-button');
+    pageIndicators.appendChild(indicator);
+  }
+}
+
+createPageIndicators();
 slide();
