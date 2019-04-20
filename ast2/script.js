277a47b27e7cf0e1b1a748c4f98d81d0cddc16b0
bigya <bigyabajracharya@gmail.com>
2019-04-20 14:10:37 +0545

    Polish code ordering

--- ast2/script.js
+++ ast2/script.js
@@ -1,14 +1,27 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
 const SCROLLBAR_HEIGHT = 5;
 const SCROLLBAR_WIDTH = 5;
-const numOfBalls = 50;
+const numOfCircles = 50;
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 canvas.style.height = window.innerHeight - SCROLLBAR_HEIGHT + 'px';
 canvas.style.width = window.innerWidth - SCROLLBAR_WIDTH + 'px';
+
+var getRandomX = () => {
+  return Math.random() * (canvas.width - 2 * radius) + radius;
+};
+
+var getRandomY = () => {
+  return Math.random() * (canvas.height - 2 * radius) + radius;
+};
+
+var circleArray = [];
+var n = 0;
+var tempCircleArray = [];
 
 // Circle class
 function Circle(x, y, radius, dx, dy) {
@@ -34,11 +47,11 @@ function Circle(x, y, radius, dx, dy) {
   };
 
   this.getRandomColor = () => {
-    this.ballColor = `rgb(${this.redValue},${this.greenValue},${
+    this.CircleColor = `rgb(${this.redValue},${this.greenValue},${
       this.blueValue
     })`;
 
-    return this.ballColor;
+    return this.CircleColor;
   };
 
   this.reverseDx = () => {
@@ -74,19 +87,10 @@ function Circle(x, y, radius, dx, dy) {
     }
   };
 }
+//End of Circle class
 
-function getRandomX() {
-  return Math.random() * (canvas.width - 2 * radius) + radius;
-}
-
-function getRandomY() {
-  return Math.random() * (canvas.height - 2 * radius) + radius;
-}
-
-var circleArray = [];
-
-// Add properties of each circle
-for (let index = 0; index < numOfBalls; index++) {
+// Initialize  each circle
+for (let index = 0; index < numOfCircles; index++) {
   var radius = Math.random() * 100;
   var x = getRandomX();
   var y = getRandomY();
@@ -98,36 +102,16 @@ for (let index = 0; index < numOfBalls; index++) {
   circleArray.push(circle);
 }
 
-// Animate balls
-function animateBall() {
-  requestAnimationFrame(animateBall);
+// Animate Circles
+function animateCircle() {
+  requestAnimationFrame(animateCircle);
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   for (var index = 0; index < circleArray.length; index++) {
     circleArray[index].drawCircle();
     circleArray[index].moveCircle();
     circleArray[index].detectWallCollision();
-
-    if (index < circleArray.length - 1) {
-      var base = Math.abs(
-        circleArray[index].getX() - circleArray[index + 1].getX()
-      );
-      var perpendicular = Math.abs(
-        circleArray[index].getY() - circleArray[index + 1].getY()
-      );
-      var hypotenuse = Math.sqrt(
-        Math.pow(base, 2) + Math.pow(perpendicular, 2)
-      );
-
-      if (
-        hypotenuse ===
-        circleArray[index].getRadius() + circleArray[index + 1].getRadius()
-      ) {
-        circleArray[index].reverseDx();
-        circleArray[index].reverseDy();
-      }
-    }
   }
 }
 
-animateBall();
+animateCircle();
